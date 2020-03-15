a6304d726867f7838cf7e3c9a4d25e3993ac5a99
unknown <ohndaniel@gmail.com>
2020-03-14 20:36:04 -0700

    Add sprites from graphics.  Add bullets and shooting at cursor.

--- js/game.js
+++ js/game.js
@@ -11,10 +11,11 @@ var config = {
   scene: {
     preload: preload,
     create: create,
-    update: update,
+    update: update
   }
 };
 
+let player;
 let hp = 45;
 let bullet;
 
@@ -29,36 +30,52 @@ let control = false;
 
 var game = new Phaser.Game(config);
 
+let white = 0xffffff;
+
 function preload() {
-  this.load.image("player", "images/square.png");
+  // this.load.image("player", "images/square.png");
+  let texture;
 
   this.graphics = this.add.graphics();
 
-  this.graphics.lineStyle(4, 0xffffff);
+  this.graphics.lineStyle(4, white);
   this.graphics.strokeCircle(400, 300, 50);
 
+  texture = this.graphics.generateTexture("player");
+
+  this.graphics.destroy();
+
+  this.graphics = this.add.graphics();
+  player = this.add.sprite(400, 300, "player");
+
+  this.graphics = this.add.graphics();
+  this.graphics.fillStyle(white, 1);
+  this.graphics.fillCircle(400, 300, 5);
+  texture = this.graphics.generateTexture("bullet");
+  bullet = this.add.sprite(400, 300, "bullet");
+
+  this.graphics.destroy();
+
+  this.graphics = this.add.graphics();
   this.graphics.fillStyle(0x90ee90, 1);
+  this.graphics.fillCircle(400, 300, hp);
 }
 
 function create() {
-
-  this.graphics.fillCircle(400, 300, hp);
-
-  bullet = this.physics.add.sprite(400, 300, 'player');
+  bullet = this.physics.add.sprite(400, 300, "bullet");
 
   input = this.input;
   mouse = input.mousePointer;
-  
-  worldBounds = this.physics.world.bounds;
 
+  worldBounds = this.physics.world.bounds;
 }
 
 function update() {
   if (game.input.activePointer.isDown) {
-    if (!mouseTouchDown) {
+    if (!mouseTouchDown && control == false) {
       touchDown();
       control = true;
-      bullet = this.physics.add.sprite(400, 300, 'player');
+      bullet = this.physics.add.sprite(400, 300, "bullet");
       this.physics.moveTo(bullet, input.x, input.y, 500);
     } else {
       if (mouseTouchDown) {
@@ -67,7 +84,12 @@ function update() {
     }
   }
 
-  if (bullet.x > worldBounds.width || bullet.y > worldBounds.height || bullet.x < 0  || bullet < 0) {
+  if (
+    bullet.x > worldBounds.width ||
+    bullet.y > worldBounds.height ||
+    bullet.x < 0 ||
+    bullet < 0
+  ) {
     control = false;
   }
 
